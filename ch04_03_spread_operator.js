7e19ce9ce0ee893d73ea1b0148418074ff9becf4
joohhcse <joohhcse@gmail.com>
2022-03-21 10:41:06 +0900

    spread operator

--- ch04_03_spread_operator.js
+++ ch04_03_spread_operator.js
@@ -1,3 +1,17 @@
+// Spread syntax(...)
+ // ES2015 추가된 syntax // 병합, 구조 분배 할당(destructuring) 등엥 다양하게 활용
+
+ const pets = ['dog', 'cat']
+ const predators = ['wolf', 'cougar']
+ const animals = [...pets, ...predators]
+ console.log(animals)    // 'dog', 'cat', 'wolf', 'cougar'
+
+const [head, ...rest] = [1, 2, 3]
+ console.log(head)    // 1
+ console.log(rest)    // [2, 3]
+
+/***********************************************************************/
+
 const ary = [1, 2, 3, 4, 5]
 
 const [head, ...rest] = ary
@@ -6,46 +20,52 @@ console.log(head, rest) // 1 [ 2, 3, 4, 5 ]
 
 console.log(head, ...rest) // 1 2 3 4 5
 
+//
+
 const personalData = {
-    email: 'abc@def.com',
-    password: '****',
-}
+     email: 'abc@def.com',
+     password: '****',
+ }
 
 const publicData = {
-    nickname: 'foo'
-}
+     nickname: 'foo'
+ }
 
 const overrides = {
-    email: 'fff@fff.com',
-}
+     email: 'fff@fff.com',
+ }
 
 const user = {
-    ...personalData,
-    ...publicData,
-}
+     ...personalData,
+     ...publicData,
+ }
 
 console.log(user) // { email: 'abc@def.com', password: '****', nickname: 'foo' }
 
-const shouldOverride = true    // or false
+/***********************************************************************/
+
+const shouldOverride = true    // or false    // syntax 활용
 
 const user2 = {
-    ...{
-        eamil: 'abc@def.com',
-        password: '****',
-    },
-    ...{
-        nickname: 'foo',
-    },
-    ...ary(shouldOverride
-        ? {
-            email: 'fff@fff.com',
-        }
-        : null),
-}
+     ...{
+         eamil: 'abc@def.com',
+         password: '****',
+     },
+     ...{
+         nickname: 'foo',
+     },
+     ...ary(shouldOverride
+         ? {
+             email: 'fff@fff.com',
+         }
+         : null),
+ }
+
+/***********************************************************************/
 
 function foo(head, ...rest) {
-    console.log(head)
-    console.log(rest)
-}
+     console.log(head)
+     console.log(rest)
+ }
 
 foo(1, 2, 3, 4)     // 1 [ 2, 3, 4]
