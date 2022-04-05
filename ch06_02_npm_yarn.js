c569128b04f95bf103b9f40ce04cf62b2409c161
joohhcse <joohhcse@gmail.com>
2022-04-05 21:23:50 +0900

    npm, yarn manager

---
+++ ch06_02_npm_yarn.js
@@ -0,0 +1,16 @@
+
+// #npm, yarn 패키지 매니저 & package.json(2)
+
+// npm install -g yarn
+// yarn add decamelize
+// yarn remove decamelize
+// 
+// npm install --save-dev eslint
+// yarn add -D eslint
+// yarn run lint
+// yarn lint
+
+
+
+
+
