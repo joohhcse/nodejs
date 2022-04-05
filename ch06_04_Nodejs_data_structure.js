7fc3f39ba39b7f8c7917bbcb08b09e6558dca6ce
joohhcse <joohhcse@gmail.com>
2022-04-05 21:50:23 +0900

    nodejs data structure

---
+++ ch06_04_Nodejs_data_structure.js
@@ -0,0 +1,18 @@
+
+// @ts-check
+
+// 1 byte = 8 bit는 0 이상 255 이하의 값 0~2^8-1
+// LE : Little Endian
+// BE : Big Endian
+
+// node buffer type
+//  
+
+// node stream type
+//  createReadStream
+
+
+
+
+
+
