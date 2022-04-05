e7a7fd19b2c0f4b82045a7e27b9102207ee59457
joohhcse <joohhcse@gmail.com>
2022-04-05 21:50:49 +0900

    nodejs standard library

---
+++ ch06_06_standard_libaray.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+//os
+const os = require('os')
+
+console.log(
+    ['arch', os.arch()],
+    ['platform', os.platform()],
+    ['cpus', os.cpus()]
+)
+
+
+//child_process
+//      dns.lookup
+//      path.resolve
+
+
+
+
