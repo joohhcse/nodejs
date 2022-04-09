9d900e79fca66ca40aaf04b23010369c9decfdb0
joohhcse <joohhcse@gmail.com>
2022-04-09 22:20:13 +0900

    simple RESTful API server

--- api.js
+++ api.js
@@ -1,6 +1,23 @@
-// API서버만들기_06_가독성과 유지보수성을 살리는 리펙토링01
+// ch05_#API서버만들기_06_가독성과 유지보수성을 살리는 리펙토링01
+// ch05_#API서버만들기_06_가독성과 유지보수성을 살리는 리펙토링02
+// ch05_#API서버만들기_07_JSON 파일을 데이터베이스로 활용해 마무리하기
+// json 파일을 DB로 활용하면서 라우트기능도 노드의 기본기능을 활용해 짜본 예제
 
 // @ts-check
+
+/** @type {Post[]} */
+const posts = [
+    {
+        id: 'my_first_post',
+        title: 'My first post',
+        const: 'Hello!',
+    },
+    {
+        id: 'my_second_post',
+        title: 'My second post',
+        content: 'Second post!',
+    },
+]
 
 /**
  * @typedef APIResponse
@@ -23,17 +40,15 @@ const routes = [
     callback: async () => ({
         // TODO: implement
         statusCode: 200,
-        body: {},
+        body: posts,
     }),
     },
 
     {
-        url: '/posts/:id', // TODO: RegExp로 고쳐야 함
+        url: /^\/posts\/([a-zA-Z0-9-_]+)$/,
         method: 'GET',
         callback: async () => ({
-            // TODO: implement
-            statusCode: 200,
-            body: {},
+
         }),
     },
 
